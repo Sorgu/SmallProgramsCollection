2010364738b9ebde6ee9b84054fd9b6b625baae6
Sorgu <32864491+Sorgu@users.noreply.github.com>
2022-03-24 11:35:26 +0100

    Update FindStartEndIndex.py

--- FindStartEndIndex.py
+++ FindStartEndIndex.py
@@ -1,3 +1,7 @@
+# Function that takes a list of intgers, and a single intger. 
+# The function attempts to find the first sequence of intgers in the list 
+# that adds up to the single intger, and then return the first and last index of the sequence. 
+# For example if the input is [1, 3, 8, 10], 11 then the output would be 1, 2 since 3 and 8 add up to 11.
 def solution(l, t):
     amount = len(l)
     i = 0
